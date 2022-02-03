9bb10db8a58e4568e67386a5ffb935e175d9f36a
DianaSeitbaeva <diana.seytbaeva@mail.ru>
2022-02-03 20:20:06 +0500

    new defs in admin and model

--- apps/new_app/admin.py
+++ apps/new_app/admin.py
@@ -1,10 +1,22 @@
+from typing import Optional
+
+from django.core.handlers.wsgi import WSGIRequest
 from django.contrib import admin
 from .models import Account
 
 class AccountAdmin(admin.ModelAdmin):
     readonly_fields = (
     )
-    
+
+    def get_readonly_fields(
+        self,
+        request: WSGIRequest,
+        obj: Optional[Account] = None
+    ) -> tuple:
+        if not obj:
+            return self.readonly_fields + ('description',)
+        return self.readonly_fields
+
 admin.site.register(
     Account, AccountAdmin
 )
--- apps/new_app/models.py
+++ apps/new_app/models.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Account(models.Model):
+    ACCOUNT_FULL_NAME_MAX_LENGTH = 20
     user = models.OneToOneField(
         User,
         on_delete=models.CASCADE
     )
     full_name = models.CharField(
-        max_length=20
+        max_length=ACCOUNT_FULL_NAME_MAX_LENGTH
     )
     description = models.TextField()
 
