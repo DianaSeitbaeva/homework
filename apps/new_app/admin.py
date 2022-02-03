52a9993560bff0ad18b541377c043752367cc0aa
DianaSeitbaeva <diana.seytbaeva@mail.ru>
2022-02-03 19:32:41 +0500

    new models and admin

--- apps/new_app/admin.py
+++ apps/new_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Account
 
-# Register your models here.
+class AccountAdmin(admin.ModelAdmin):
+    readonly_fields = (
+    )
+    
+admin.site.register(
+    Account, AccountAdmin
+)
--- apps/new_app/models.py
+++ apps/new_app/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Account(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE
+    )
+    full_name = models.CharField(
+        max_length=20
+    )
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f'Account: {self.user.id} / {self.full_name}'
+    
+    class Meta:
+        ordering = (
+            'full_name',
+            )
+        verbose_name = 'Аккаунт'
+        verbose_name_plural = 'Аккаунты'
