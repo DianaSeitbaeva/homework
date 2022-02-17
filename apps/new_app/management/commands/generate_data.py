bfeb3c98337b27c16b8d97798bc357bb853e185b
DianaSeitbaeva <diana.seytbaeva@mail.ru>
2022-02-17 22:51:23 +0500

    add generate students and account

--- apps/new_app/management/commands/generate_data.py
+++ apps/new_app/management/commands/generate_data.py
@@ -25,6 +25,31 @@ class Command(BaseCommand):
     def __init__(self, *args: tuple, **kwargs: dict) -> None:
         pass
 
+    def _generate_students(self) -> None:
+        STUDENT_COUNT = 100
+
+        def _generate_account(i: int) -> str:
+            return f'Пользователь{i}'
+        
+        def _generate_student(i:int) -> str:
+            return f'Студент{i}'
+
+        i: int
+        for i in range(STUDENT_COUNT):
+            account_user: str = _generate_account(i)
+            account_student: str = _generate_student(i)
+            rand_int = random.randint(0,20)
+            Student.objects.create(
+                age = random.randint(16,27),
+                gpi = random.randint(2,12),
+                group = 
+                account=Account.objects.create(
+                    user=User.objects.create(
+                        username=account_user
+                    )
+                )
+            )
+
     def _generate_groups(self) -> None:
         """Generate Group objs."""
         def generate_name(inc:int) -> str:
